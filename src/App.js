155c89ad16d198abcadf3346e1a6378b979c3e55
VeeraKoskinen <veera.koskinen@ren.to>
2018-04-25 11:29:03 +0300

    added default implementations for notifications

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends React.Component {
             title: "",
             content: "",
 
+            notification: null,
             error: null,
             visible: false
         }
@@ -37,64 +38,87 @@ class App extends React.Component {
         const events = await eventtiService.getAll()
         this.setState({ events })
 
-        const loggedUserJSON = window.localStorage.getItem('loggedEventtiappUser')
+        const loggedUserJSON = window.localStorage.getItem('loggedTrainingappUser')
         if (loggedUserJSON) {
             const user = JSON.parse(loggedUserJSON)
             this.setState({ user })
             eventtiService.setToken(user.token)
             this.setState({ name: user.name })
         }
-    }    
+    }
 
     handleFieldChange = (event) => {
         this.setState({ [event.target.name]: event.target.value })
         console.log(event.target.value)
     }
 
+
+    // ---- Login / Logout ---- //
+
     login = async (event) => {
         event.preventDefault()
         console.log('login in with: ', this.state.username, this.state.password)
-    
+
         try {
-          const user = await loginService.login({
-            username: this.state.username,
-            password: this.state.password
-          })
-    
-          window.localStorage.setItem('loggedTrainingappUser', JSON.stringify(user))
-          eventtiService.setToken(user.token)
-          
-          console.log(user)
-          this.setState({ name: user.name, username: '', password: '', user })
-          console.log('nimi asetetaan userin mukaan:')
-          console.log(this.state.name)
-          console.log(this.state.user)
-    
+            const user = await loginService.login({
+                username: this.state.username,
+                password: this.state.password
+            })
+
+            window.localStorage.setItem('loggedTrainingappUser', JSON.stringify(user))
+            eventtiService.setToken(user.token)
+
+            console.log(user)
+            this.setState({ name: user.name, username: '', password: '', user })
+            console.log('nimi asetetaan userin mukaan:')
+            console.log(this.state.name)
+            console.log(this.state.user)
+
         } catch (exception) {
-          this.updateError('Käyttäjätunnus tai salasana on virheellinen!')
-    
-          console.log(exception)
+            this.updateError('Käyttäjätunnus tai salasana on virheellinen!')
+
+            console.log(exception)
         }
     }
 
     logout = async () => {
         console.log('logout metodissa')
-    
+
         window.localStorage.removeItem('loggedTrainingappUser')
         this.setState({ user: null, username: '', password: '' })
         window.localStorage.clear()
-    
+
     }
-    
+
+
+    // ----  Errors / Notifications ----  //
+
+    updateNotification = (message) => {
+        this.setState({ notification: message })
+        setTimeout(() => {
+            this.setState({ notification: null })
+        }, 3000)
+    }
+
+    updateError = (message) => {
+        this.setState({ error: message })
+        setTimeout(() => {
+          this.setState({ error: null })
+        }, 3000)
+    }
+
 
 
     render() {
         if (this.state.user === null) {
             return (
                 <div>
-                    <LoginForm password={this.state.password} username={this.state.username} handleFieldChange={this.handleFieldChange} onSubmit={this.login}/>
+                    <div>
+                        <LoginForm password={this.state.password} username={this.state.username} handleFieldChange={this.handleFieldChange} onSubmit={this.login} />
+                    </div>
+                    <Notification message={this.state.error} className={"error"} />
                 </div>
-            )                                        
+            )
         } else {
             return (
                 <div>
@@ -103,11 +127,13 @@ class App extends React.Component {
                         <form onSubmit={this.logout}>
                             <button type="submit"> Logout </button>
                         </form>
-                    </div>    
+                    </div>
+                    <Notification message={this.state.notification} className={"notification"} />
+                    <Notification message={this.state.error} className={"error"} />
                 </div>
             )
         }
     }
-}  
+}
 
 export default App;
